c3a8eda47f6cccfb5b90c8a5816062eaa25b2016
Homygo25 <nagacclark@gmail.com>
2025-06-20 00:28:29 +0800

    Add PHP backend skeleton

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+class AuthController {
+    public function login($request) {
+        // TODO: implement login logic
+    }
+
+    public function register($request) {
+        // TODO: implement register logic
+    }
+}
---
+++ backend/app/Http/Controllers/StrategyController.php
@@ -0,0 +1,10 @@
+<?php
+class StrategyController {
+    public function index() {
+        // TODO: list strategies
+    }
+
+    public function store($request) {
+        // TODO: store new strategy
+    }
+}
